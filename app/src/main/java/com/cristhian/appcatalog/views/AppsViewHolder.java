33b1949e54af21c3b2dd9e39f2775da33edb8bfd
sca-cristhian <dar_funeral18@hotmail.com>
2016-02-07 15:19:19 -0500

    Set TextView layout margin programmatically

--- app/src/main/java/com/cristhian/appcatalog/views/AppsViewHolder.java
+++ app/src/main/java/com/cristhian/appcatalog/views/AppsViewHolder.java
@@ -1,12 +1,14 @@
 package com.cristhian.appcatalog.views;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.cristhian.appcatalog.R;
+import com.cristhian.appcatalog.activities.LaunchScreenActivity;
 
 /**
  * Created by ctolosa on 02/02/2016.
@@ -27,5 +29,14 @@ public class AppsViewHolder extends RecyclerView.ViewHolder {
         placeNameHolder = (LinearLayout) itemView.findViewById(R.id.placeNameHolder);
         imageView = (ImageView) v.findViewById(R.id.image);
         textView = (TextView) itemView.findViewById(R.id.placeName);
+
+        LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams) textView.getLayoutParams();
+        if (LaunchScreenActivity.IS_TABLET){
+            lp.setMargins(120,0,0,0);
+        }else {
+            lp.setMargins(100,0,0,0);
+        }
+
+        textView.setLayoutParams(lp);
     }
 }
